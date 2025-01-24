2c091c82328bca1d94cc7c9a25f215b440aed6d3
JJabbez <jcjabbes08@gmail.com>
2025-01-24 12:12:38 -0800

    UI wip
    Created basis for main menu and changeScene script and the pauseManager script. Will make pretty and finish after class :D

---
+++ Assets/Scripts/pauseManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//THIS SCRIPT IS FOR THE PAUSE MENUUUUUU YUHHYHHHHHH
+public class PauseManager : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    public static bool isPaused; //make global variable so no other inputs during pause
+    void Start(){
+        PauseMenu.SetActive(false);
+    }
+
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(isPaused){
+                resumeGame();
+            }else{
+                pauseGame();
+            }
+        }
+    }
+
+    public void pauseGame(){
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+    }
+
+    public void resumeGame(){
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void mainMenu(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("mainMenu");
+    }
+
+    public void quitGame(){
+        Application.Quit();
+    }
+}
+
