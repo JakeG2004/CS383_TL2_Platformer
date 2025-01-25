61d5fc9f447d125d23e1ef6e3321d1bb6ad572bc
Dristanta Silwal <dristantasilwal003@gmail.com>
2025-01-25 07:51:51 -0800

    Squashed commit of the following:
    commit f9ec609baabc09c4860033655fedaf03f5558432
    Author: wrello <officialwrello@gmail.com>
    Date:   Fri Jan 24 23:45:18 2025 -0800
    
        Update TagManager.asset
    
    commit d6624e0cfc4acfd7975b1a59f7a6284d6b895fe5
    Merge: b2102ca 167eea9
    Author: wrello <officialwrello@gmail.com>
    Date:   Fri Jan 24 23:45:09 2025 -0800
    
        Merge branch 'Sprites' of https://github.com/JakeG2004/CS383_TL2_Platformer into Sprites
    
    commit b2102cabeee5e7755b97f0db77a11da5a6fdba5d
    Author: wrello <officialwrello@gmail.com>
    Date:   Fri Jan 24 23:43:50 2025 -0800
    
        map + coin that gets destroyed on touch
    
    commit 167eea94d3e0c7f5227a4ec1dd7e6a3d061fffd9
    Author: Collin Worth <worthcollin@gmail.com>
    Date:   Fri Jan 24 21:59:44 2025 -0800
    
        removed old code
    
    commit 2cdbe891dfb404f9600a4e2b6c48bac1fec88ff6
    Author: Collin Worth <worthcollin@gmail.com>
    Date:   Fri Jan 24 21:47:06 2025 -0800
    
        random commit
    
    commit 72d6b465ee8365859ea4442011023b5e0db3ea1d
    Author: wrello <officialwrello@gmail.com>
    Date:   Fri Jan 24 21:44:33 2025 -0800
    
        map assets
    
    commit 4e0cc13b566a693a2df5439b6fb1bc3a39816a62
    Author: Collin Worth <worthcollin@gmail.com>
    Date:   Fri Jan 24 21:20:43 2025 -0800
    
        issacs coin
    
    commit fad49f4336f432bca14b0f458e5e393cf53567b9
    Author: Collin Worth <worthcollin@gmail.com>
    Date:   Fri Jan 24 20:24:53 2025 -0800
    
        resolved floating feet from idle animation
    
    commit e889d73177af7af3b016d5c546c8a456a4badd1d
    Author: Collin Worth <worthcollin@gmail.com>
    Date:   Fri Jan 24 20:24:39 2025 -0800
    
        added hurt animation
    
    commit 38b0e20c49141dfced73b54d18c3ea9b96c967e0
    Author: Collin Worth <worthcollin@gmail.com>
    Date:   Fri Jan 24 20:12:19 2025 -0800
    
        fixed flip direction (not bad animation point for knight)
    
    commit 4d8703b42b0e506786eb56a098f0ce01be0f5f80
    Author: Collin Worth <worthcollin@gmail.com>
    Date:   Fri Jan 24 19:50:11 2025 -0800
    
        new filp
    
    commit 270d191ffe373e3b6a63adfdd752a073bf7442ae
    Author: Collin Worth <worthcollin@gmail.com>
    Date:   Fri Jan 24 19:31:18 2025 -0800
    
        fliping sprite animator changes
    
    commit b68c884e0bf8d4c5f5d19a44cba169ce98750c1d
    Author: Collin Worth <worthcollin@gmail.com>
    Date:   Fri Jan 24 16:21:44 2025 -0800
    
        Introduced new sprite and set up clips for animation
    
    commit 951fc66b7cfb8ebd2298a15b69a47fd20ca96e36
    Author: Collin Worth <worthcollin@gmail.com>
    Date:   Fri Jan 24 08:04:41 2025 -0800
    
        init commit for Sprites Branch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,39 +1,27 @@
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     // Keys
+    [SerializeField] private KeyCode _left = KeyCode.A;
+    [SerializeField] private KeyCode _right = KeyCode.D;
     [SerializeField] private KeyCode _jump = KeyCode.W;
-
-    // Variable to get x axis for movement From input manager
-    private float _move;
 
     // useful values to change
     [SerializeField] private float _maxSpeed = 10.0f;
     [SerializeField] private float _jumpForce = 8.0f;
     [SerializeField] private float _friction = 10.0f;
-    [SerializeField] private float _fallThreshold = -10.0f;
-
 
     // Private variables
     private Rigidbody2D _rb = null;
     private bool _isGrounded = false;
 
-    //Starting position
-    private Vector2 _startingPosition;
-
-    //Health
-    public Image healthBar;
-    public CanvasGroup gameOverCanvas;
-    private float healthAmount = 100f;
-    private float damage = 50f;
-
-    //BC Mode
-    private bool bcMode = false;
-
+    // Animator
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    
+    bool facingRight = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -46,22 +34,9 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Failed to get rigidbody!");
         }
 
-        
-        
-
-        //Save starting position
-        _startingPosition = transform.position;
-
-        //Initialize health bar
-        healthBar.fillAmount = healthAmount / 100f;
-
-        //Initially make game over canvas invisible
-        gameOverCanvas.alpha = 0f;
-        gameOverCanvas.interactable = false;
-        gameOverCanvas.blocksRaycasts = false;
-
-        //Load BC mode setting check (jillian)
-        UpdateBCMode();
+        // Assign Animator component
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -69,59 +44,41 @@ public class PlayerController : MonoBehaviour
     {
         GetPlayerMovement();
         CheckGround();
-
-        UpdateBCMode(); //checks if exist (jillian)
-
-        if(Input.GetKeyDown("r") || Input.GetButtonDown("Submit"))
-        {
-            ExitGameOver();
-        }
-
-
-        if(transform.position.y < _fallThreshold || healthAmount <= 0)
-        {
-            ResetToStart();
-            TriggerGameOver();
-
-            /*
-            if(bcMode)
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            }
-            else
-            {
-                ResetToStart();
-                TriggerGameOver();
-            }
-            */
-        }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            Application.Quit();
-        }
     }
 
     void GetPlayerMovement()
     {
         // Handle horizontal movement
-        _move = Input.GetAxis("Horizontal");
-        if(_move != 0)
+        if(Input.GetKey(_left))
         {
-           _rb.linearVelocity = new Vector2(_move*_maxSpeed,_rb.linearVelocity.y);
-         
+            _rb.linearVelocityX = -1 * _maxSpeed;
+            
+            if(facingRight){
+                Flip();
+            }
+            animator.SetBool("IsRunning", true);
         }
-        
+
+        else if(Input.GetKey(_right))
+        {
+            _rb.linearVelocityX = _maxSpeed;
+            if(!facingRight){
+                Flip();
+            }
+            animator.SetBool("IsRunning", true);
+        }
+
         // Lerp to zero velocity
         else
         {
             _rb.linearVelocityX = Mathf.Lerp(_rb.linearVelocityX, 0.0f, Time.deltaTime * _friction);
+            animator.SetBool("IsRunning", false);
         }
 
         // Handle vertical movement
-        if((Input.GetKeyDown(_jump) || Input.GetButtonDown("Jump")) && _isGrounded)
+        if(Input.GetKeyDown(_jump) && _isGrounded)
         {
-            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _jumpForce);
-            
+            _rb.linearVelocityY = _jumpForce;
         }
     }
 
@@ -140,11 +97,15 @@ public class PlayerController : MonoBehaviour
             if(groundDist < (transform.localScale.y / 2) + 0.1f)
             {
                 _isGrounded = true;
+
+                // End animation for jumping
+                animator.SetBool("IsJumping", false);
             }
 
             else
             {
                 _isGrounded = false;
+                animator.SetBool("IsJumping", true);
             }
         }
 
@@ -153,52 +114,19 @@ public class PlayerController : MonoBehaviour
             _isGrounded = false;
         }
     }
+
+    void Flip(){
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
+    }
+
     public void Hurt()
     {
-        if(!bcMode){
-        //Change player's color to red upon impact
-        GetComponent<SpriteRenderer>().color = Color.red;
-
-        TakeDamage(damage);
-        }
+        // Populate
+        Debug.Log("Player hurt");
+        animator.SetTrigger("TriggerHurt");
     }
-
-    public void TakeDamage(float damage)
-    {
-        if(!bcMode){ //(jillian)
-            healthAmount -= damage;
-            healthAmount = Mathf.Clamp(healthAmount, 0, 100);
-            healthBar.fillAmount = healthAmount / 100f;
-            _maxSpeed = 7f;
-        }
-    }
-
-    private void UpdateBCMode(){
-        bcMode = PlayerPrefs.GetInt("BCMode", 0) == 1;
-    }
-    void TriggerGameOver()
-    {
-        //gameOverCanvas.alpha = 1f;
-        //gameOverCanvas.interactable = true;
-        //gameOverCanvas.blocksRaycasts = true;
-
-        SceneManager.LoadScene(2); //(andrew)
-
-    }
-
-    void ExitGameOver()
-    {
-        gameOverCanvas.alpha = 0f;
-        gameOverCanvas.interactable = false;
-        gameOverCanvas.blocksRaycasts = false;
-    }
-
-    void ResetToStart()
-    {
-        transform.position = _startingPosition;
-
-        //Reset velocity to prevent momentum
-        _rb.linearVelocity = Vector2.zero;
-    }
-
 }
