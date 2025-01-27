2bfe18908a47c5fe9094819a51e3865f166e7eec
Tavin Yorgason <tazasor@gmail.com>
2025-01-27 00:01:06 -0800

    Disable mobile UI if not on mobile
    Also added a couple other sprites to the mobileUI sprite folder in case more buttons are added

--- Assets/Scripts/MobileUI.cs
+++ Assets/Scripts/MobileUI.cs
@@ -5,11 +5,7 @@ public class MobileUI : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log(Application.platform);
-        if( Application.platform != RuntimePlatform.Android )
-        {
-            //gameObject.SetActive(false);
-        }
+        gameObject.SetActive(Application.isMobilePlatform);
     }
 
     // Update is called once per frame
